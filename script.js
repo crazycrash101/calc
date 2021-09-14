78211cc78fa536201c454e1609cd4d33673b3cc7
sameer <sameer.ghawana@gmail.com>
2021-09-14 12:16:41 +0530

    complete

--- script.js
+++ script.js
@@ -4,31 +4,79 @@
 let upperScreen = document.querySelector(".upper");
 let lowerScreen = document.querySelector(".lower");
 let ac = document.querySelector("#ac");
-let buttons = document.querySelectorAll(".row:not(.row1):not(.row5) > div,row5 > #zero");
+let buttons = document.querySelectorAll("#one,#two,#three,#four,#five,#six,#seven,#eight,#nine,#zero");
 let backspace = document.querySelector("#backspace");
 let decimal = document.querySelector("#decimal");
 let operators = document.querySelectorAll("#plus,#minus,#multiply,#divide");
 let equals = document.querySelector("#equals");
 
+/* important variables */
 let content="";
+let contentNum = 0;
+
+let firstNum = 0;
+let secondNum = 0;
+let operation= "";
+let result = "";
+
+
+/* important functions */
+
+function operate(num1,num2,operator) {
+    if(operator === "+"){
+        return (num1+num2).toFixed(2);
+    } else if(operator === "-"){
+        return (num1-num2).toFixed(2);
+    } else if(operator === "*"){
+        return (num1*num2).toFixed(2);
+    } else if(operator === "/"){
+        return (num1/num2).toFixed(2);
+    }
+}
+
+function showContent() {
+    upperScreen.textContent = content;
+}
+
+function resetContent() {
+    content = "0";
+    contentNum = 0;
+    upperScreen.textContent = content;
+    lowerScreen.textContent = "";
+}
+
+function resetCalc () {
+    firstNum = 0;
+    secondNum = 0;
+    operation = "";
+    result = "";
+}
+
+
 
 
 
 /* input rules for digit buttons */
 buttons.forEach(div => {
     div.addEventListener("click",e => {
-        if(content.length < 12) {
-            if(content == 0){
-                content = div.textContent
+        if(upperScreen.textContent.length < 12) {
+            if(upperScreen.textContent === "0"){
+                content = div.textContent;
+                upperScreen.textContent = div.textContent;
+                contentNum = Number(content);
             } else {
                 content += div.textContent;
+                upperScreen.textContent += div.textContent;
+                contentNum = Number(content);
             }
-        } else if (content.length == 12){
-            content += div.textContent;
-            let temp = content.split("").slice(1,content.length).join("");
+        } else if (upperScreen.textContent.length == 12){
+            upperScreen.textContent += div.textContent;
+            let temp = upperScreen.textContent.split("").slice(1,upperScreen.textContent.length).join("");
             content = temp;
+            upperScreen.textContent = temp;
+            contentNum = Number(content);
         }
-        showContent();
+        
     })
 })
 
@@ -36,20 +84,26 @@ buttons.forEach(div => {
 /* input rules for AC button*/
 ac.addEventListener("click",e => {
     resetContent();
+    resetCalc();
+
 })
 
 
 /* input rules for backspace button*/
 backspace.addEventListener("click",e => {
-    if (content.length == 1) {
-        if (content != "0") {
-            content = "0"
+    if (upperScreen.textContent.length == 1) {
+        if (upperScreen.textContent != "0") {
+            upperScreen.textContent = "0"
+            content = "0";
+            contentNum = Number(content);
         }
     } else {
-        let temp = content.split("").slice(0,(content.length-1)).join("");
+        let temp = upperScreen.textContent.split("").slice(0,(upperScreen.textContent.length-1)).join("");
+        upperScreen.textContent = temp;
         content = temp;
+        contentNum = Number(content);
     }
-    showContent();
+    //showContent();
 })
 
 
@@ -61,73 +115,53 @@ decimal.addEventListener("click",e => {
             toggle = false;
         }
     }
-    if(content.length < 12) {
+    if(upperScreen.textContent.length < 12) {
         if(toggle === true){
+            upperScreen.textContent += decimal.textContent;
             content += decimal.textContent;
+            contentNum = Number(content);
+            //showContent();
         }
-    } else if (content.length === 12){
+    } else if (upperScreen.textContent.length === 12){
         if(toggle === true){
-            content += decimal.textContent;
-            let temps = content.split("").slice(2,content.length-1).join("");
+            upperScreen.textContent += decimal.textContent;
+            let temps = upperScreen.textContent.split("").slice(1,upperScreen.textContent.length).join("");
+            upperScreen.textContent = temps;
             content = temps;
-            showContent()
+            contentNum = Number(content);
+            
+            //showContent();
         }
     }
 })
 
 /* input rules for operators */
 
-let firstNum = 0;
-let secondNum = 0;
-let operation= "";
-let result = "";
-
 operators.forEach(div => {
     div.addEventListener("click",e => {
-        firstNum = content;
-        resetContent();
-        operation = div.textContent;
-        showContent();
+        if (lowerScreen.textContent === ""){
+            firstNum = contentNum;
+            upperScreen.textContent += div.textContent;
+            operation = div.textContent;
+            content = "0";
+            contentNum = Number(content); 
+        } else {
+            firstNum = Number(lowerScreen.textContent);
+            upperScreen.textContent = lowerScreen.textContent;
+            upperScreen.textContent += div.textContent;
+            operation = div.textContent;
+            content = "0";
+            contentNum = Number(content);
+        }
     })
 })
 
 /* input rules for equals */
 equals.addEventListener("click", e => {
-    secondNum = content;
-    resetContent()
-    result = String(operate(Number(firstNum),Number(secondNum),operation));
+    secondNum = contentNum;
+    result = operate(firstNum,secondNum,operation);
+    firstNum = result;
     lowerScreen.textContent = result;
-    resetCalc;
 })
 
 
-
-
-
-function operate(num1,num2,operator) {
-    if(operator === "plus"){
-        return (num1+num2);
-    } else if(operator === "minus"){
-        return (num1-num2);
-    } else if(operator === "multiply"){
-        return (num1*num2);
-    } else if(operator === "divide"){
-        return (num1/num2);
-    }
-}
-
-function showContent() {
-    upperScreen.textContent = content;
-}
-
-function resetContent() {
-    content = "0";
-    upperScreen.textContent = content;
-}
-
-function resetCalc () {
-    firstNum = 0;
-    secondNum = 0;
-    operation = "";
-    result = "";
-}
